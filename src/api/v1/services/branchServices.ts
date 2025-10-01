bdcf44edf10595e76a5c44361385eeb561e023ff
madam31-rrc <madam31@academic.rrc.ca>
2025-10-01 00:53:57 -0500

    add branches to track changes

---
+++ src/api/v1/services/branchServices.ts
@@ -0,0 +1,12 @@
+import branches, { Branch } from "../data/branchData"
+import branchs from "../data/branchData"
+
+export const createBranch = async (branch: {
+    name: string,
+    address: string,
+    phone: string,
+}): Promise<Branch> => {
+    const newBranch: Branch = { id: Date.now().toString(), ...branch };
+    branches.push(newBranch);
+    return newBranch;
+};
